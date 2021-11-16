7aea69f3cb46a17540c9f4baad876c626ed56abb
kcx1 <casey@ascendstudios.com>
2021-11-16 16:34:51 -0500

    Updated to be consistent with other modules that modify exisiting objects

--- Advanced_String.lua
+++ Advanced_String.lua
@@ -1,3 +1,14 @@
-function string.trim(s)
+--[[
+
+Advanced string functions that are not implemented in LUA
+
+  To use:
+    require("Advanced_Tables")
+    Then call as expeted - table.reverse(tbl)
+
+--]]
+function string.trim (s)
   return s:match'^%s*(.*)'
 end
+
+return string
