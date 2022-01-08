f8ba9a11fbcf82773b3d4ef0fdea19ea587ab4b6
kcx1 <casey@ascendstudios.com>
2022-01-08 12:04:29 -0500

    localizes function variables, fixes typos.Prints when pages are handled.

--- UCI_Script_Manager.lua
+++ UCI_Script_Manager.lua
@@ -5,8 +5,8 @@ util = require "Utilities"
 main = Component.New("UCI Environment")
 touch_timer = Timer.New()
 
-function filter_contorls(ctls, prefix)
-  filtered = {}
+function filter_controls(ctls, prefix)
+  local filtered = {}
   for name, ctl in pairs(ctls) do
     if string.find(name, prefix) then
       filtered[string.trim(string.sub(name, #prefix +1))] = ctl
@@ -16,10 +16,9 @@ function filter_contorls(ctls, prefix)
 end
 
 function other_controls(ctls)
-  filtered = {}
+  local filtered = {}
   for name, ctl in pairs(ctls) do
-    if not string.find(name, "LAYERS:")  or not string.find(name, "PAGES:") then
-      table.insert(filtered, ctl)
+    if not string.find(name, "LAYERS:")  or not string.find(name, "PAGE:") then
     end
   end
 end
@@ -37,6 +36,7 @@ end
 function page_events(ctls)
   for name, ctl in pairs(ctls) do
     ctl.EventHandler = function()
+      print('HANDLER\t', name, ctl, ctls[name].Boolean)
       if ctl.Boolean then main.Pages.String = name end
       util.exclude(ctl, ctls)
     end
@@ -52,8 +52,8 @@ function other_events(ctls)
 end
 
 
-Layer_Controls = filter_contorls(Controls, "LAYER:")
-Page_Controls = filter_contorls(Controls, "PAGE:")
+Layer_Controls = filter_controls(Controls, "LAYER:")
+Page_Controls = filter_controls(Controls, "PAGE:")
 Other_Controls = other_controls(Controls)
 
 
