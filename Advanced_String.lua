9c0402208382bbef89cc2d14c844dbd4f42a1d3b
kcx1 <casey@ascendstudios.com>
2022-01-09 12:54:54 -0500

    Localizes variables. Adds in ability to use regex instead of plain text. Updates position to use ending of the sep variable instead of using length

--- Advanced_String.lua
+++ Advanced_String.lua
@@ -11,16 +11,18 @@ function string.trim (s)
   return s:match'^%s*(.*)'
 end
 
-function string.split(self, sep)
-  list = {}
-  position = 1
-  index = string.find(self, sep)
+function string.split(self, sep, regex)
+  local list = {}
+  local position = 1
+  if not regex then mode = "plain" end
+  local index, z = string.find(self, sep, 1, mode)
   while index do
     table.insert(list, string.sub(self, position, index -1))
-    position = index + 1
-    index = string.find(self, sep, position)
+    position = z + 1
+    index, z = string.find(self, sep, position, mode)
   end
   table.insert(list, string.sub(self, position, -1))
+  if #list == 0 then list = self end
   return list
 end
 
