f2f5fdb4ab9fba9942333aadd91ed4c63b8ec489
kcx1 <casey@ascendstudios.com>
2022-01-09 01:01:42 -0500

    Adds python-like string.split method

--- Advanced_String.lua
+++ Advanced_String.lua
@@ -11,4 +11,17 @@ function string.trim (s)
   return s:match'^%s*(.*)'
 end
 
+function string.split(self, sep)
+  list = {}
+  position = 1
+  index = string.find(self, sep)
+  while index do
+    table.insert(list, string.sub(self, position, index -1))
+    position = index + 1
+    index = string.find(self, sep, position)
+  end
+  table.insert(list, string.sub(self, position, -1))
+  return list
+end
+
 return string
