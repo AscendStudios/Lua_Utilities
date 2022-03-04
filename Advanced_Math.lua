e0960433e0b79684f12fcd003420eb8787d0ee6d
kcx1 <casey@ascendstudios.com>
2022-03-04 13:16:33 -0500

    Adds sum function and clamp function

--- Advanced_Math.lua
+++ Advanced_Math.lua
@@ -13,3 +13,24 @@ function math.round(num, numDecimalPlaces)
   local mult = 10^(numDecimalPlaces or 0)
   return math.floor(num * mult + 0.5) / mult
 end
+
+function math.sum(...)
+  args = {...}
+  local sum = 0
+  for i=1, #args do
+    sum = sum + args[i]
+  end
+  return sum
+end
+
+
+function math.clamp(num, min, max)
+  assert(min < max, "Min number must be less than the Max")
+  if min <= num and max >= num then
+    return num
+  elseif num > max then
+    return max
+  else
+    return min
+  end
+end
